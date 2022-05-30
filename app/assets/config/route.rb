61d74dfbac5b17735ca115373c5d0e5f06353775
Samuel Coizet <messa57fr@gmail.com>
2022-05-30 12:25:57 +0200

    ex2

---
+++ app/assets/config/route.rb
@@ -0,0 +1,3 @@
+  # Generic syntax:
+  # verb 'path', to: 'controller#action'
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  def ask
+  end
+
+  def answer
+    ask = params[:question]
+    @answer = ''
+    if ask == 'I am going to work'
+      @answer = 'Great!'
+    elsif ask.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+    @answer
+  end
+end
